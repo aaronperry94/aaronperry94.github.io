35575a937cc0845ad309e672fb0e771a8138130c
aaronperry94 <45040057+aaronperry94@users.noreply.github.com>
2019-09-02 14:43:59 +0100

    second commit
    changed background img

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,7 +1,9 @@
 let resumeData = {
   name: "Aaron Perry",
   role: "Computing Systems Graduate",
-  linkedinId: " https://www.linkedin.com/in/aaron-perry-031300182/",
+  linkedinId: "  https://www.linkedin.com/in/aaron-perry-031300182/",
+  email: "  aaronperry.94@hotmail.com",
+  mobile: "  07568501690",
   roleDescription:
     " I am passionate for Software/Web development, and I feel I would be a valuable asset to any team. I like to practice new technologies and develop my skills in my spare time.  ",
   socialLinks: [
@@ -16,51 +18,70 @@ let resumeData = {
       className: "fa fa-github"
     }
   ],
-  aboutme: "I have Graduated from Ulster University in July 2019. ",
+  aboutme:
+    "My name is Aaron Perry, and I have recently graduated from Ulster University in July 2019. I am actively in search of a graduate opportunity within Software/Web Development. I am passionate and ambitious, and I love making applications look good! In particular, using front-end technologies such as jQuery and React make this possible. In my spare time, I have also started learning back-end tech such as Node.js. Outside of web design, I like to practice my software dvelopment skills, such as Java and C++.",
   address: "Newtownabbey, Northern Ireland",
   website: "https://aaronperry94.github.io",
   education: [
     {
-      UniversityName: "The LNM Insitute of Information Technology",
-      specialization: "Some specialization",
-      MonthOfPassing: "Aug",
-      YearOfPassing: "2020",
-      Achievements: "Some Achievements"
+      UniversityName: "Ulster University",
+      specialization: "BSc Hons Computing Systems",
+      MonthOfPassing: "July",
+      YearOfPassing: "2019",
+      Achievements: "Computing Systems"
     },
     {
-      UniversityName: "Some University",
-      specialization: "Some specialization",
-      MonthOfPassing: "Jan",
-      YearOfPassing: "2018",
-      Achievements: "Some Achievements"
+      UniversityName: "Northern Regional College",
+      specialization: "Higher National Certificate",
+      MonthOfPassing: "August",
+      YearOfPassing: "2014",
+      Achievements: "Computing Systems w/ Games Development"
     }
   ],
   work: [
     {
-      CompanyName: "Some Company",
-      specialization: "Some specialization",
-      MonthOfLeaving: "Jan",
-      YearOfLeaving: "2018",
+      CompanyName: "Dixons Carphone (Currys PC World)",
+      specialization: "Sales Team Leader",
+      MonthOfLeaving: "April 2016 - Present",
       Achievements: "Some Achievements"
     },
     {
-      CompanyName: "Some Company",
-      specialization: "Some specialization",
-      MonthOfLeaving: "Jan",
-      YearOfLeaving: "2018",
+      CompanyName: "McDonalds",
+      specialization: "Staff Trainer",
+      MonthOfLeaving: "December",
+      YearOfLeaving: "2015",
       Achievements: "Some Achievements"
     }
   ],
-  skillsDescription: "Your skills here",
+  skillsDescription:
+    "The skills below are reflective of a graduate level. Every day through learning, I strive to develop this skill set.",
   skills: [
     {
       skillname: "HTML5"
     },
     {
-      skillname: "CSS"
+      skillname: "CSS3"
     },
     {
-      skillname: "Reactjs"
+      skillname: "PHP"
+    },
+    {
+      skillname: "SQL"
+    },
+    {
+      skillname: "Java"
+    },
+    {
+      skillname: "JavaScript"
+    },
+    {
+      skillname: "ReactJS"
+    },
+    {
+      skillname: "NodeJS"
+    },
+    {
+      skillname: "jQuery"
     }
   ],
   portfolio: [
@@ -83,16 +104,6 @@ let resumeData = {
       name: "project4",
       description: "mobileapp",
       imgurl: "images/portfolio/phone.jpg"
-    }
-  ],
-  testimonials: [
-    {
-      description: "This is a sample testimonial",
-      name: "Some technical guy"
-    },
-    {
-      description: "This is a sample testimonial",
-      name: "Some technical guy"
     }
   ]
 };
