91838a8acaebcfdf2c788e726215ef61364d15ed
aaronperry94 <45040057+aaronperry94@users.noreply.github.com>
2019-09-02 12:30:00 +0100

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+  name: "Aaron Perry",
+  role: "Computing Systems Graduate",
+  linkedinId: " https://www.linkedin.com/in/aaron-perry-031300182/",
+  roleDescription:
+    " I am passionate for Software/Web development, and I feel I would be a valuable asset to any team. I like to practice new technologies and develop my skills in my spare time.  ",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/aaron-perry-031300182/",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "https://github.com/aaronperry94",
+      className: "fa fa-github"
+    }
+  ],
+  aboutme: "I have Graduated from Ulster University in July 2019. ",
+  address: "Newtownabbey, Northern Ireland",
+  website: "https://aaronperry94.github.io",
+  education: [
+    {
+      UniversityName: "The LNM Insitute of Information Technology",
+      specialization: "Some specialization",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2020",
+      Achievements: "Some Achievements"
+    },
+    {
+      UniversityName: "Some University",
+      specialization: "Some specialization",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2018",
+      Achievements: "Some Achievements"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
+    },
+    {
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5"
+    },
+    {
+      skillname: "CSS"
+    },
+    {
+      skillname: "Reactjs"
+    }
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg"
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png"
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg"
+    }
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    }
+  ]
+};
+
+export default resumeData;
